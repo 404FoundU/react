3e7051ccf233b214a6e752186050335ca231f467
upillai <unnikrishnanrp@outlook.com>
2021-05-21 12:24:28 -0500

    Added counter2

--- render-props/src/App.js
+++ render-props/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Wrapper from "./Wrapper";
 import Counter1 from "./Counter1";
+import Counter2 from "./Counter2";
 
 function App() {
   return (
     <div className="App">
-<Wrapper render={(count, incCount)=> <Counter1 count={count} incCount={incCount}/>}/>
+<Wrapper render={(count, incCount)=> {
+    return <div>
+        <Counter1 count={count} incCount={incCount}/>
+    </div>
+}}/><Wrapper render={(count, incCount)=> {
+    return <div>
+        <Counter2 count={count} incCount={incCount}/>
+    </div>
+}}/>
     </div>
   );
 }
--- render-props/src/Counter2.js
+++ render-props/src/Counter2.js
@@ -1,7 +1,10 @@
+
 import React from "react";
 
-const Counter2 = () =>{
-
+const Counter2 = ({count, incCount}) =>{
+    return (
+        <button onClick={incCount}>{count}</button>
+    )
 }
 
 export default Counter2;
