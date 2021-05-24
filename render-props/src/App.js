ac2972e9f58aa63b8017a4f6664d455921000ce9
upillai <unnikrishnanrp@outlook.com>
2021-05-24 14:33:46 -0500

    init

--- render-props/src/App.js
+++ render-props/src/App.js
@@ -4,19 +4,20 @@ import Counter1 from "./Counter1";
 import Counter2 from "./Counter2";
 
 function App() {
-  return (
-    <div className="App">
-<Wrapper render={(count, incCount)=> {
-    return <div>
-        <Counter1 count={count} incCount={incCount}/>
-    </div>
-}}/><Wrapper render={(count, incCount)=> {
-    return <div>
-        <Counter2 count={count} incCount={incCount}/>
-    </div>
-}}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Wrapper render={(count, incCount) => {
+                return <div>
+                    <Counter1 count={count} incCount={incCount}/>
+                </div>
+            }}/>
+            <Wrapper render={(count, incCount) => {
+            return <div>
+                <Counter2 count={count} incCount={incCount}/>
+            </div>
+        }}/>
+        </div>
+    );
 }
 
 export default App;
